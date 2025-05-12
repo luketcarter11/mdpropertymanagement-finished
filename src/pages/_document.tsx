748a14a7580d99c8011b673b3d438f1f6db8248d
BLprojects-1 <BLclipproject@gmail.com>
2025-05-12 21:28:08 +0100

    Added simplified favicon implementation

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,10 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* Simple Favicon */}
+        <link rel="icon" type="image/png" href="/favicon.png" />
+
+        {/* Fonts */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
